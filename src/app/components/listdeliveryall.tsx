a207c870b8d1735ea2d46347239497705fa88700
kluis6 <luislmorningstar@gmail.com>
2024-11-12 06:16:38 -0300

    e

--- src/app/components/listdeliveryall.tsx
+++ src/app/components/listdeliveryall.tsx
@@ -13,52 +13,31 @@ export default function ListDeliveryAll() {
   return (
     <>
       <ul className="transition-all divide-y">
-        {items.map((value, index ) =>(<li key={index} className={"bg-blue-500 relative"} onClick={()=>toggleAccordion(index) }>
-          <section className="justify-between items-center flex p-4">
-            <p className="font-medium">Entrega</p>{" "}
-            {drop === index ? (
-              <MdExpandLess className="text-xl shrink-0 text-neutral-50" />
-            ) : (
-              <MdExpandMore className="text-xl text-neutral-300 shrink-0" />
-            )}
-          </section>
-          <section
-            className={`bg-neutral-50 w-full transition-all ${
-              drop === index ? "h-48" : "h-0"
-            }`}
+        {items.map((value, index) => (
+          <li
+            key={index}
+            className={"bg-blue-500 relative"}
+            onClick={() => toggleAccordion(index)}
           >
-            <article className={` ${drop === index ? " p-4" : "hidden"}`}>sdsdsd</article>
-          </section>
-        </li>))}
-        <li className={"bg-blue-500 relative"} onClick={() => setDrop(!drop)}>
-          <section className="justify-between items-center flex p-4">
-            <p className="font-medium">Entrega</p>{" "}
-            {drop ? (
-              <MdExpandLess className="text-xl shrink-0 text-neutral-50" />
-            ) : (
-              <MdExpandMore className="text-xl text-neutral-300 shrink-0" />
-            )}
-          </section>
-          <section
-            className={`bg-neutral-50 w-full transition-all ${
-              drop ? "h-48" : "h-0"
-            }`}
-          >
-            <article className={` ${drop ? " p-4" : "hidden"}`}>sdsdsd</article>
-          </section>
-        </li>
-        <li className={"bg-blue-500 relative"} onClick={() => setDrop(!drop)}>
-          <section className="justify-between items-center flex p-4">
-            <p className="font-medium">Entrega</p>
-          </section>
-          <section
-            className={`bg-neutral-50 w-full transition-all ${
-              drop ? "h-48" : "h-0"
-            }`}
-          >
-            <article className={` ${drop ? "" : "hidden"}`}>sdsdsd</article>
-          </section>
-        </li>
+            <section className="justify-between items-center flex p-4">
+              <p className="font-medium">Entrega</p>{" "}
+              {drop === index ? (
+                <MdExpandLess className="text-xl shrink-0 text-neutral-50" />
+              ) : (
+                <MdExpandMore className="text-xl text-neutral-300 shrink-0" />
+              )}
+            </section>
+            <section
+              className={`bg-neutral-50 w-full transition-all ${
+                drop === index ? "h-48" : "h-0"
+              }`}
+            >
+              <article className={` ${drop === index ? " p-4" : "hidden"}`}>
+                sdsdsd
+              </article>
+            </section>
+          </li>
+        ))}
       </ul>
     </>
   );
